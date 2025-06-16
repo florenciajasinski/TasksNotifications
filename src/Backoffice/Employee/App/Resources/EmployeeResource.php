76e2a5727789f430e0d1f5f7c81b30dad1a9ebba
Florencia Jasinski <florenciajasinski@Florencias-MacBook-Pro.local>
2025-06-16 15:56:26 -0300

    fix: employee resource

--- src/Backoffice/Employee/App/Resources/EmployeeResource.php
+++ src/Backoffice/Employee/App/Resources/EmployeeResource.php
@@ -7,17 +7,18 @@ namespace Lightit\Backoffice\Employee\App\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @property-read \Lightit\Backoffice\Employee\Domain\Models\Employee $resource
+ */
 class EmployeeResource extends JsonResource
 {
-    /**
-     * @return array{id: int, name: string, email: string}
-     */
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
-            'name' => $this->name,
-            'email' => $this->email,
+            'id' => $this->resource->id,
+            'name' => $this->resource->name,
+            'email' => $this->resource->email,
         ];
     }
 }
+
