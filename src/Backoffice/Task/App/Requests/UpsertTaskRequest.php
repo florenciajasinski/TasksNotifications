096a11083c6e5641b1e9c7bd9327ad07ab5b8339
Florencia Jasinski <florenciajasinski@Florencias-MacBook-Pro.local>
2025-06-18 16:39:54 -0300

    fix: fix  phpstan

--- src/Backoffice/Task/App/Requests/UpsertTaskRequest.php
+++ src/Backoffice/Task/App/Requests/UpsertTaskRequest.php
@@ -42,7 +42,7 @@ class UpsertTaskRequest extends FormRequest
         return new TaskDto(
             title: $this->string(self::TITLE)->toString(),
             description: $this->string(self::DESCRIPTION)->toString(),
-            status: $this->enum(self::STATUS, TaskStatus::class) ?? TaskStatus::from($this->input(self::STATUS)),
+            status: $this->enum(self::STATUS, TaskStatus::class) ?? TaskStatus::from($this->string(self::STATUS)->toString()),
             employeeId: $this->integer(self::EMPLOYEE_ID),
             taskId: $this->integer(self::TASK_ID)
         );
